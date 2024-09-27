0d8113d46f3381183c67965cee4a27553e46c4f2
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2024-09-27 20:40:13 +0200

    heron app

---
+++ heron.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Please insert a number!\n";
+	double user_input;
+	double x;
+	double y;
+	std::cin >> user_input;
+	x = user_input/2;
+	y = (x+2)/2;
+	while (1==1){
+		x = (x+y)/2;
+		std::cout << x << '\n';
+		y = user_input/x;
+		std::cout << y << '\n';
+	}
+
+		return 0;
+}
