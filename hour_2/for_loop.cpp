e35f084f1bc6b3f77c836bd330a6f972e5912768
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2024-09-29 19:39:08 +0200

    learn for loops

---
+++ hour_2/for_loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+	for(int i = 1; i <= 3; i++){ // i = index
+		std::cout << "HAPPY NEW YEAR!\n";
+	}
+
+	for(int i = 1; i <= 10; i+=2){
+		std::cout << i << '\n';
+	}
+
+	std::cout << "HAPPY NEW YEAR!\n";
+
+	for(int i = 10; i >= 0; i-=2){
+		std::cout << i << '\n';
+
+	}
+
+return 0;
+}
